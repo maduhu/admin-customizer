15c1ae91f0bcadf9b07da2e1f3f2f34a4ada6a43
Nilambar Sharma <nilambar@outlook.com>
2016-01-16 20:00:39 +0545

    fix upload control

--- npf-framework/fields/upload.php
+++ npf-framework/fields/upload.php
@@ -17,11 +17,15 @@ if ( ! class_exists('npf_field_upload')):
 		{
 
 			$field_value = $this->get_value($args);
+            $style = '';
+            if ( empty( $field_value ) ) {
+                $style = 'style="display:none;"';
+            }
 			echo '<input type="text" name="'.$args['field_name'].'" id="'.$args['field_id'].'" value="'.$field_value.'" class="regular-text code img" />';
 			echo '<input type="button" class="select-img button button-primary" value="Upload" data-uploader_button_text="Select" data-uploader_title="'.$args['field']['title'].'" />';
-			echo '<div class="image-preview-wrap" >';
+			echo '<div class="image-preview-wrap" ' . $style . '>';
 				echo '<div class="image-preview-left-part">';
-					echo '<img src="'.$field_value.'" alt="Preview" class="img-preview" />';
+					echo '<img src="'.$field_value.'" alt="" class="img-preview" />';
 				echo '</div><!-- .image-preview-left-part -->';
 				echo '<div class="image-preview-right-part">';
 					echo '<input type="button" class="button btn-remove-upload" value="Remove" />';
